9248a17b660f9523a98ee14621693e68908648bd
Аnna <admin@MacBook-Pro-Anna.local>
2022-10-03 19:26:55 +0300

    finalvers

--- class_practice_copy/Program.cs
+++ class_practice_copy/Program.cs
@@ -63,4 +63,32 @@ else
 }
 */
 
-//
+// Написать программу , которая на вход приниммает координаты 2 точек 2 и находит расстояние м-ду ними в 2д пространстве
+// Math.Sqrt
+// Console.WriteLine("d={d:f3});
+
+/*
+Console.WriteLine("Введите кооординаты точки x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кооординаты точки x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+Console.WriteLine($"расстояние = {result:f3}");
+*/
+
+// Написать программу, которая на вход принимает число и выдает таблицу квадратов чисел от 1 до н
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int index = 1; index <= num; index ++)
+{
+    double result = Math.Pow(index,2);
+    Console.Write($"{result} ");
+}
+
